da62b6fa517002554ab18e3a0d3495aa521ba2b8
MatveyD <matv1995@gmail.com>
2018-03-14 08:41:16 +0300

    hw4-task3

---
+++ src/HomeWork4/Task3/Jvisualvm.java
@@ -0,0 +1,28 @@
+package HomeWork4.Task3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Home task 3.
+ *
+ * jvisualvm
+ */
+public class Jvisualvm {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+
+        int objectCounter = 0; // counter of objects eligible tp GC
+
+        while (true) {
+            list.add(new Object());
+            objectCounter++;
+            if (objectCounter % 100_000_000 == 0) {
+                System.gc();
+            }
+
+        }
+
+    }
+}
